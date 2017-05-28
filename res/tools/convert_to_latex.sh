99d8a570db00189142791ca4953487e960adf5f2
Mitch Stevens <mitchstevens95@gmail.com>
2017-05-28 16:54:49 +1000

    added tool to convert xml documentation to latex

---
+++ res/tools/convert_to_latex.sh
@@ -0,0 +1,15 @@
+#!/bin/bash/
+scalac XML2Latex.scala
+cat "../xml/levels.xml" | scala XML2Latex > levels.tex
+latex levels.tex
+pdflatex levels.tex
+
+mv levels.pdf "../docs"
+mv levels.tex "../docs"
+
+rm levels.aux
+rm levels.dvi
+rm levels.log
+rm levels.toc
+rm XML2Latex.class
+rm "XML2Latex$.class"
---
+++ src/main/scala/core/types/GameInformation.scala
@@ -0,0 +1,8 @@
+package core.types
+
+/**
+  * Created by Mitch on 5/26/2017.
+  */
+class GameInformation {
+
+}
